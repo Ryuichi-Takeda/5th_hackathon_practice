c542de1ce9172941c5d6eda0def5124243bd351f
fukuba-shuma <calculus.fukuba@gmail.com>
2022-08-20 12:02:36 +0900

    draw htmls

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const readUser = async () => {
-    const { data } = await axios.get('http://localhost/index.php')
-    return data
-}
+  const { data } = await axios.get("http://localhost/index.php");
+  return data;
+};
+
+const drawHTML = async () => {
+  const res = await readUser();
+  const data = Object.entries(res).map(d => d[1])
+  let result = ``;
+  data.forEach((element) => {
+    const { name, age, id } = element;
+    let text = `
+        <td class="name">${name}</td>
+        <td class="age">${age}</td>
+        <td class="id">${id}</td>`;
+    result += text;
+  });
+  let target = document.getElementById("draw_point");
+  target.innerHTML = result;
+};
 
 window.onload = async () => {
-    const data = await readUser()
-    console.log(data)
-}
+  drawHTML();
+};
